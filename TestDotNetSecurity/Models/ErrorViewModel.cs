3a6401ca84d011e8f96a06b5af4aa16e41b6d943
Tom Ordille <tordille@gmail.com>
2022-02-28 08:13:30 -0500

    Add project files.

---
+++ TestDotNetSecurity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestDotNetSecurity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
